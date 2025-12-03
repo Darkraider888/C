44c2d2f3bd383827cfe47ed63fe4a8396d26be35
Mehedi Hasan Siddique <hasanmehidi2016@gmail.com>
2025-12-03 19:55:30 +0600

    Create Hello World Program.C

---
+++ Hello World Program.C
@@ -0,0 +1,15 @@
+// Include the standard input-output library
+// This provides functions like printf() for printing output
+#include <stdio.h>
+
+// Every C program must have a main() function
+// Execution of the program starts from here
+int main() {
+    // printf() displays the string inside quotation marks
+    // \n is the newline character - moves cursor to next line
+    printf("Hello, World!\n");
+    
+    // Return 0 indicates successful program execution
+    // Non-zero values usually indicate errors
+    return 0;
+}
